0eb43bbec8bad5df7651bfc8495ebbcccceed3f1
Hwang-Jaeryeong <jryeong67@naver.com>
2024-02-25 01:06:11 +0900

    [스택, 큐, 덱] 2월 25일

--- 01_정렬_맵_셋/14425.cpp
+++ 01_정렬_맵_셋/14425.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-map<string, bool> M;
-
 int main(){
     int n, m;
     string str;
     int res = 0;
     cin >> n >> m;
+    map<string, bool> M;
 
     for (int i = 0; i < n; i++){
         cin >> str;
